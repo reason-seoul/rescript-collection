5c6f6081f567f14c95fa8bb075a203b7df25b1aa
Hyunwoo Nam <namenu@gmail.com>
2021-01-17 15:25:09 +0900

    Updates

--- normalize.e750914d.js
+++ normalize.e750914d.js
@@ -218,7 +218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57912" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60050" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
